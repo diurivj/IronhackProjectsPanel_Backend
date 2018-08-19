3b0f924f7b860c78bfc50173c431515ebfecec11
diurivj <diurivj@icloud.com>
2018-08-19 12:42:54 -0500

    creating endpoint for update cohort with a new student is created

--- routes/cohort.js
+++ routes/cohort.js
@@ -9,9 +9,13 @@ router.get('/all', (req, res) => {
   Cohort.find().then(result => res.json(result)).catch(error => console.log(error))
 })
 
-router.get('/:id/students/', (req, res) => {
+router.get('/:id/students', (req, res) => {
   Cohort.findById(req.params.id).populate('students')
   .then(result => res.json(result)).catch(error => console.log(error))
+})
+
+router.patch('/update/:id', (req, res, next) => {
+  Cohort.findByIdAndUpdate(req.params.id, {$push: {students: req.body.students}}, {new: true}).then(result => res.json(result)).catch(error => console.log(error))
 })
 
 module.exports = router
